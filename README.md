09f58a1813a666efea58a6d44770b830db859989
Seongyong Kim <sy@Seongyongs-MacBook-Pro-3.local>
2023-01-28 04:10:05 -0500

    upload previous reference

--- README.md
+++ README.md
@@ -1,1 +1,16 @@
 # Vehicle-Proximity-Detection
+
+[**Datasets**](https://www.dropbox.com/scl/fo/m92awm24eghe5vem413wh/h?dl=0&rlkey=fe5hex5xovvq8597tu4o4tkw9)
+
+<details open>
+<summary>Previous</summary>
+
+**Preliminary**  
+https://www.dropbox.com/sh/fnnw9opwmnkfifi/AACj8UTHG30UiI-lkhtfkEOua?dl=0
+
+**Reference**  
+Yolov5  https://github.com/ultralytics/yolov5  
+DeepSort https://github.com/ZQPei/deep_sort_pytorch
+</details>
+
+
---
+++ previous/SpecialProblem-Arshiya/track.py
@@ -0,0 +1,376 @@
+# limit the number of cpus used by high performance libraries
+import os
+os.environ["OMP_NUM_THREADS"] = "1"
+os.environ["OPENBLAS_NUM_THREADS"] = "1"
+os.environ["MKL_NUM_THREADS"] = "1"
+os.environ["VECLIB_MAXIMUM_THREADS"] = "1"
+os.environ["NUMEXPR_NUM_THREADS"] = "1"
+
+import sys
+sys.path.insert(0, './yolov5')
+
+import argparse
+import os
+import platform
+import shutil
+import time
+from pathlib import Path
+import cv2
+import torch
+import torch.backends.cudnn as cudnn
+import numpy as np
+import winsound
+import subprocess
+from gtts import gTTS
+from pydub import AudioSegment
+AudioSegment.converter = "C:/ffmpeg/bin/ffmpeg.exe"
+
+
+from yolov5.models.experimental import attempt_load
+from yolov5.utils.downloads import attempt_download
+from yolov5.models.common import DetectMultiBackend
+from yolov5.utils.datasets import LoadImages, LoadStreams
+from yolov5.utils.general import (LOGGER, check_img_size, non_max_suppression, scale_coords, 
+                                  check_imshow, xyxy2xywh, increment_path)
+from yolov5.utils.torch_utils import select_device, time_sync
+from yolov5.utils.plots import Annotator, colors
+from deep_sort.utils.parser import get_config
+from deep_sort.deep_sort import DeepSort
+
+FILE = Path(__file__).resolve()
+ROOT = FILE.parents[0]  # yolov5 deepsort root directory
+if str(ROOT) not in sys.path:
+    sys.path.append(str(ROOT))  # add ROOT to PATH
+ROOT = Path(os.path.relpath(ROOT, Path.cwd()))  # relative
+
+def distance_esti(bboxes, cls):
+    bbox_h = bboxes[3] - bboxes[1]
+    f = 250
+
+    # if cls == 0:
+    #     original_height = 62 #in inch for person
+    if cls == 1:
+        original_height = 42 #in inch for bicycle
+    elif cls == 2:
+        original_height = 60 #in inch for car
+    elif cls == 3:
+        original_height = 45 #in inch for motorcycle
+    elif cls == 5:
+        original_height = 160 #in inch for bus
+    elif cls == 7:
+        original_height = 160 #in inch for truck
+
+    dist = (original_height * f)/ abs(bbox_h)
+    dist = dist/12
+
+    return dist
+
+# def warning():
+#     winsound.Beep(440, 500)
+#     Annotator.warning()
+
+def detect(opt):
+    out, source, yolo_model, deep_sort_model, show_vid, save_vid, save_txt, imgsz, evaluate, half, project, name, exist_ok= \
+        opt.output, opt.source, opt.yolo_model, opt.deep_sort_model, opt.show_vid, opt.save_vid, \
+        opt.save_txt, opt.imgsz, opt.evaluate, opt.half, opt.project, opt.name, opt.exist_ok
+    webcam = source == '0' or source.startswith(
+        'rtsp') or source.startswith('http') or source.endswith('.txt')
+
+    device = select_device(opt.device)
+    # initialize deepsort
+    cfg = get_config()
+    cfg.merge_from_file(opt.config_deepsort)
+    deepsort = DeepSort(deep_sort_model,
+                        device,
+                        max_dist=cfg.DEEPSORT.MAX_DIST,
+                        max_iou_distance=cfg.DEEPSORT.MAX_IOU_DISTANCE,
+                        max_age=cfg.DEEPSORT.MAX_AGE, n_init=cfg.DEEPSORT.N_INIT, nn_budget=cfg.DEEPSORT.NN_BUDGET,
+                        )
+
+    # Initialize
+    half &= device.type != 'cpu'  # half precision only supported on CUDA
+
+    # The MOT16 evaluation runs multiple inference streams in parallel, each one writing to
+    # its own .txt file. Hence, in that case, the output folder is not restored
+    if not evaluate:
+        if os.path.exists(out):
+            pass
+            shutil.rmtree(out)  # delete output folder
+        os.makedirs(out)  # make new output folder
+
+    # Directories
+    if type(yolo_model) is str:
+        exp_name = yolo_model.split(".")[0]
+    elif type(yolo_model) is list and len(yolo_model) == 1:
+        exp_name = yolo_model[0].split(".")[0]
+    else:
+        exp_name = "ensemble"
+    exp_name = exp_name + "_" + deep_sort_model.split('/')[-1].split('.')[0]
+    save_dir = increment_path(Path(project) / exp_name, exist_ok=exist_ok)  # increment run if project name exists
+    save_dir.mkdir(parents=True, exist_ok=True)  # make dir
+
+    # Load model
+    device = select_device(device)
+    model = DetectMultiBackend(yolo_model, device=device, dnn=opt.dnn)
+    stride, names, pt, jit, _ = model.stride, model.names, model.pt, model.jit, model.onnx
+    imgsz = check_img_size(imgsz, s=stride)  # check image size
+
+    # Half
+    half &= pt and device.type != 'cpu'  # half precision only supported by PyTorch on CUDA
+    if pt:
+        model.model.half() if half else model.model.float()
+
+    # Set Dataloader
+    vid_path, vid_writer = None, None
+    # Check if environment supports image displays
+    if show_vid:
+        show_vid = check_imshow()
+
+    # Dataloader
+    if webcam:
+        show_vid = check_imshow()
+        cudnn.benchmark = True  # set True to speed up constant image size inference
+        dataset = LoadStreams(source, img_size=imgsz, stride=stride, auto=pt and not jit)
+        bs = len(dataset)  # batch_size
+
+    else:
+        dataset = LoadImages(source, img_size=imgsz, stride=stride, auto=pt and not jit)
+        bs = 1  # batch_size
+    vid_path, vid_writer = [None] * bs, [None] * bs
+
+    # Get names and colors
+    names = model.module.names if hasattr(model, 'module') else model.names
+
+    # extract what is in between the last '/' and last '.'
+    txt_file_name = source.split('/')[-1].split('.')[0]
+    txt_path = str(Path(save_dir)) + '/' + txt_file_name + '.txt'
+
+    if pt and device.type != 'cpu':
+        model(torch.zeros(1, 3, *imgsz).to(device).type_as(next(model.model.parameters())))  # warmup
+    dt, seen = [0.0, 0.0, 0.0, 0.0], 0
+    prev_outputs = None
+    dist_prev = []
+    first = True
+    tts = AudioSegment.from_file(r"beep-02.wav")
+    #tts = winsound.Beep(2500,100)
+    for frame_idx, (path, img, im0s, vid_cap, s) in enumerate(dataset):
+        dist1 = []
+        t1 = time_sync()
+        img = torch.from_numpy(img).to(device)
+        img = img.half() if half else img.float()  # uint8 to fp16/32
+        img /= 255.0  # 0 - 255 to 0.0 - 1.0
+        if img.ndimension() == 3:
+            img = img.unsqueeze(0)
+        t2 = time_sync()
+        dt[0] += t2 - t1
+
+        # Inference
+        visualize = increment_path(save_dir / Path(path[0]).stem, mkdir=True) if opt.visualize else False
+        pred = model(img, augment=opt.augment, visualize=visualize)
+        t3 = time_sync()
+        dt[1] += t3 - t2
+
+        # Apply NMS
+        pred = non_max_suppression(pred, opt.conf_thres, opt.iou_thres, opt.classes, opt.agnostic_nms, max_det=opt.max_det)
+        dt[2] += time_sync() - t3
+
+        # Process detections
+        for i, det in enumerate(pred):  # detections per image
+            seen += 1
+            if webcam:  # batch_size >= 1
+                p, im0, _ = path[i], im0s[i].copy(), dataset.count
+                s += f'{i}: '
+            else:
+                p, im0, _ = path, im0s.copy(), getattr(dataset, 'frame', 0)
+
+            p = Path(p)  # to Path
+            save_path = str(save_dir / p.name)  # im.jpg, vid.mp4, ...
+            #save_path = str("video.mp4")
+            s += '%gx%g ' % img.shape[2:]  # print string
+
+            annotator = Annotator(im0, line_width=2, pil=not ascii)
+
+            if det is not None and len(det):
+                # Rescale boxes from img_size to im0 size
+                det[:, :4] = scale_coords(
+                    img.shape[2:], det[:, :4], im0.shape).round()
+
+                # Print results
+                for c in det[:, -1].unique():
+                    n = (det[:, -1] == c).sum()  # detections per class
+                    s += f"{n} {names[int(c)]}{'s' * (n > 1)}, "  # add to string
+
+                xywhs = xyxy2xywh(det[:, 0:4])
+                confs = det[:, 4]
+                clss = det[:, 5]
+
+                # pass detections to deepsort
+                t4 = time_sync()
+                outputs = deepsort.update(xywhs.cpu(), confs.cpu(), clss.cpu(), im0)
+                t5 = time_sync()
+                dt[3] += t5 - t4
+
+                texts = []
+
+                # draw boxes for visualization
+                if len(outputs) > 0: 
+                
+                    for j, (output, conf) in enumerate(zip(outputs, confs)):
+
+                        bboxes = output[0:4]
+                        id = output[4]
+                        cls = output[5]
+
+                        c = int(cls)  # integer class
+                        co = c
+                        dist = distance_esti(bboxes,cls) # distance estimation
+                        dist1 = np.append(dist1,dist)
+                        
+                        if np.all(prev_outputs) != None and (id in prev_outputs[:,4]):
+                            ind = np.where(prev_outputs[:,4]== id)
+                            vel = float((dist_prev[ind] - dist)*30) #velocity estimation assuming 30 FPS sampling
+                            vel = vel*0.6818 #conversion to miles/hr
+                            t_col = dist/vel
+
+                            if t_col<3:
+                                co = 21
+                                label = f'{names[c]} D={dist:.2f}ft V={vel:.3f}mph WARNING'
+                                texts = "warning"
+                            else:
+                                label = f'{names[c]} D={dist:.2f}ft V={vel:.3f}mph'
+
+                            # if vel > 45 and t_col <=5:
+                            #     annotator.warning()
+                            # elif vel<45 and t_col <=3 and t_col>0:
+                            #     annotator.warning()
+                        else:
+                            label = f'{names[c]} D={dist:.2f}ft'
+
+                        annotator.box_label(bboxes, label, color=colors(co, True))
+                        
+                        # if save_txt:
+                        #     # to MOT format
+                        #     bbox_left = output[0]
+                        #     bbox_top = output[1]
+                        #     bbox_w = output[2] - output[0]
+                        #     bbox_h = output[3] - output[1]
+                        #     # Write MOT compliant results to file
+                        #     with open(txt_path, 'a') as f:
+                        #         f.write(('%g ' * 10 + '\n') % (frame_idx + 1, id, bbox_left,  # MOT format
+                        #                                        bbox_top, bbox_w, bbox_h, -1, -1, -1, -1))
+                    dist_prev = dist1
+                    k = int(len(dist1))
+                    prev_outputs = outputs[0:k,:]
+
+                LOGGER.info(f'{s}Done. YOLO:({t3 - t2:.3f}s), DeepSort:({t5 - t4:.3f}s)')
+
+            else:
+                deepsort.increment_ages()
+                prev_outputs = None
+                dist_prev = dist1
+                LOGGER.info('No detections')
+
+            # Stream results
+            im0 = annotator.result()
+            if show_vid:
+                cv2.imshow(str(p), im0)
+                if cv2.waitKey(1) == ord('q'):  # q to quit
+                    raise StopIteration
+
+            print(save_dir, save_path)
+            #print(save_path)
+            # Save results (image with detections)
+
+            if save_vid:
+    
+                if vid_path != save_path:  # new video
+                    vid_path = save_path
+
+                    if isinstance(vid_writer, cv2.VideoWriter):
+                        vid_writer.release()  # release previous video writer
+                    if vid_cap:  # video
+                        fps = vid_cap.get(cv2.CAP_PROP_FPS)
+                        w = int(vid_cap.get(cv2.CAP_PROP_FRAME_WIDTH))
+                        h = int(vid_cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
+                    else:  # stream
+                        fps, w, h = 30, im0.shape[1], im0.shape[0]
+                        #fps, w, h = 5, im0.shape[1], im0.shape[0]
+
+                    #vid_writer = cv2.VideoWriter(save_path, cv2.VideoWriter_fourcc(*'mp4v'), fps, (w, h))
+                    vid_writer = cv2.VideoWriter(r"video.mp4", cv2.VideoWriter_fourcc(*'mp4v'), fps, (w, h))
+
+                if len(texts) > 0 and frame_idx%3==0:
+                    #silence = AudioSegment.silent(duration=0.01*33.33)
+                    #description = ', '.join(texts)
+                    #tts = gTTS(texts, lang='en')
+                    #tts.save('tts.mp3')
+                    #tts = AudioSegment.from_mp3("tts.mp3")
+                    
+                    if first:
+                        audio = tts
+                    else:
+                        audio = AudioSegment.from_mp3(r"audio.mp3")
+                        audio = audio + tts
+                else:
+                    silence = AudioSegment.silent(duration=100) #100milliseconds for 3 frames
+                    if first:
+                        audio = silence
+                    else:
+                        audio = AudioSegment.from_mp3(r"audio.mp3")
+                        audio = audio + silence
+
+                audio.export(r"audio.mp3", format="mp3")
+                print("entered")
+                first = False
+
+                vid_writer.write(im0)
+
+    # Print results
+    vid_writer.release()
+    
+    cv2.destroyAllWindows()
+    
+    cmd = 'ffmpeg -i video.mp4 -i audio.mp3 -c copy output.mp4'
+    subprocess.call(cmd, shell=True)
+    t = tuple(x / seen * 1E3 for x in dt)  # speeds per image
+    LOGGER.info(f'Speed: %.1fms pre-process, %.1fms inference, %.1fms NMS, %.1fms deep sort update \
+        per image at shape {(1, 3, *imgsz)}' % t)
+    if save_txt or save_vid:
+        print('Results saved to %s' % save_path)
+        if platform == 'darwin':  # MacOS
+            os.system('open ' + save_path)
+
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--yolo_model', nargs='+', type=str, default='yolov5m.pt', help='model.pt path(s)')
+    parser.add_argument('--deep_sort_model', type=str, default='osnet_ibn_x1_0_MSMT17')
+    parser.add_argument('--source', type=str, default='0', help='source')  # file/folder, 0 for webcam
+    parser.add_argument('--output', type=str, default='inference/output', help='output folder')  # output folder
+    parser.add_argument('--imgsz', '--img', '--img-size', nargs='+', type=int, default=[640], help='inference size h,w')
+    parser.add_argument('--conf-thres', type=float, default=0.5, help='object confidence threshold')
+    parser.add_argument('--iou-thres', type=float, default=0.5, help='IOU threshold for NMS')
+    parser.add_argument('--fourcc', type=str, default='mp4v', help='output video codec (verify ffmpeg support)')
+    parser.add_argument('--device', default='', help='cuda device, i.e. 0 or 0,1,2,3 or cpu')
+    parser.add_argument('--show-vid', action='store_true', help='display tracking video results')
+    parser.add_argument('--save-vid', action='store_true', help='save video tracking results')
+    parser.add_argument('--save-txt', action='store_true', help='save MOT compliant results to *.txt')
+    # class 0 is person, 1 is bycicle, 2 is car... 79 is oven
+    parser.add_argument('--classes', nargs='+', type=int, help='filter by class: --class 0, or --class 16 17')
+    parser.add_argument('--agnostic-nms', action='store_true', help='class-agnostic NMS')
+    parser.add_argument('--augment', action='store_true', help='augmented inference')
+    parser.add_argument('--evaluate', action='store_true', help='augmented inference')
+    parser.add_argument("--config_deepsort", type=str, default="deep_sort/configs/deep_sort.yaml")
+    parser.add_argument("--half", action="store_true", help="use FP16 half-precision inference")
+    parser.add_argument('--visualize', action='store_true', help='visualize features')
+    parser.add_argument('--max-det', type=int, default=1000, help='maximum detection per image')
+    parser.add_argument('--dnn', action='store_true', help='use OpenCV DNN for ONNX inference')
+    parser.add_argument('--project', default=ROOT / 'runs/track', help='save results to project/name')
+    parser.add_argument('--name', default='exp', help='save results to project/name')
+    parser.add_argument('--exist-ok', action='store_true', help='existing project/name ok, do not increment')
+    opt = parser.parse_args()
+    opt.imgsz *= 2 if len(opt.imgsz) == 1 else 1  # expand
+
+    with torch.no_grad():
+        detect(opt)
